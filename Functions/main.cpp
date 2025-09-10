1de7ff4d5fdef2235ff356205200a3de63eefc39
Mohamed Shaban El Waaer <136377111+MohamedShabanElwa3er@users.noreply.github.com>
2025-09-10 18:11:15 +0300

    Update main.cpp

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <array>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 /*
@@ -10,15 +11,15 @@ Add `int max_of_four(int a, int b, int c, int d)` here.
 
 int max_of_four(const int a,const int b,const int c , const int d)
 {
-    std::array<int, 4> arr = {a,b,c,d};
-    auto [min,max]=std::minmax_element(arr.begin(),arr.end());
+    std::vector<int> v = {a,b,c,d};
+    auto [min,max]=std::minmax_element(v.begin(),v.end());
     return *max;
 }
 int main() {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    std::cin>>a>>b>>c>>d;
+
+    std::cout<<max_of_four(a,b,c,d);
     
     return 0;
 }
